eb3132309f4654e8d11206c055f5ae970ad798cd
Maicon Lourenço <maiconloure@gmail.com>
2024-07-09 01:07:45 -0300

    feat: added new routes and validators

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import fastify from "fastify"
+import { prisma } from "./lib/prisma"
+import { createTrip } from "./routes/create-trip"
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
 
 const app = fastify()
 
-app.get("/",()=>{
-    return "Hello World"
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createTrip)
 
 app.listen({port:3000},()=>{
     console.log("Server is running on port 3000")
